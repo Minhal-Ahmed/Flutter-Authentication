06a42df15e1b3139f12959f7960967cf04abb254
Minhal-Ahmed <minhalahmed28@gmail.com>
2024-04-17 00:50:46 +0500

    Try catch block added

--- app/lib/home_page.dart
+++ app/lib/home_page.dart
@@ -1,10 +1,89 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget{
-  const HomePage({super.key});
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<User> users = [];
+
+  Future<void> fetchUsers() async {
+    try {
+      var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body) as List<dynamic>;
+
+        List<User> fetchedUsers = jsonData.map((userJson) => User.fromJson(userJson)).toList();
+
+        setState(() {
+          users = fetchedUsers;
+        });
+      } else {
+        throw Exception('Failed to load users: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching users: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Users'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        titleTextStyle: TextStyle(
+          
+        ),
+      ),
+      body: users.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(users[index].name),
+                  subtitle: Text(users[index].email),
+                  leading: Text('${users[index].id}'), // Display user ID in the leading position
+                );
+              },
+            ),
+    );
   }
 }
